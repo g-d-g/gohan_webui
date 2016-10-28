f00ba6aaf90b14e3cb38184649d33cac6b16612c
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-10-28 09:29:20 +0200

    Added support for undefined option in select menu.

--- app/js/views/formsEditors/selectEditor.js
+++ app/js/views/formsEditors/selectEditor.js
@@ -14,17 +14,33 @@ class SelectEditor extends Backbone.Form.editors.Select {
       container: 'body',
       width: '100%'
     };
+
     const optionLength = Object.keys(this.schema.options).length;
 
     if ((this.schema.options instanceof Array)) {
-      this.setOptions(this.schema.options);
+      const options = new Array(...this.schema.options);
+
+      if (this.schema.nullable && !this.schema.validators.includes('required')) {
+        this.setOptions(options.reduce((result, item) => {
+          result[item] = item;
+
+          return result;
+        }, {'': 'Not selected'}));
+      } else {
+        this.setOptions(options);
+      }
     } else {
       const sortedOptions = Object.keys(this.schema.options)
         .sort((a, b) => this.schema.options[a].localeCompare(this.schema.options[b]))
         .reduce((acc, val) => {
           acc[val] = this.schema.options[val];
           return acc;
-        }, {});
+        }, (() => {
+          if (this.schema.nullable && !this.schema.validators.includes('required')) {
+            return {'': 'Not selected'};
+          }
+          return {};
+        })());
 
       this.setOptions(sortedOptions);
     }
